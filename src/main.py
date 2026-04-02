d06ce95582598e5c43f324ed53eec79330d3e89c
causedcrawdad <gabrielangarita67@gmail.com>
2026-04-01 20:09:59 -0400

    .

--- src/main.py
+++ src/main.py
@@ -11,14 +11,29 @@ def main(page: ft.Page):
     page.floating_action_button = ft.FloatingActionButton(
         icon=ft.Icons.ADD, on_click=increment_click
     )
+
+    semesterbar = ft.MenuBar(
+        controls=[
+            ft.SubmenuButton(
+                content=ft.Text("Submenu"),
+                controls=[
+                    ft.MenuItemButton(content=ft.Text("Item 1")),
+                    ft.MenuItemButton(content=ft.Text("Item 2")),
+                    ft.MenuItemButton(content=ft.Text("Item 3")),
+                ],
+            ),
+        ],
+    )
+
     page.add(
+        semesterbar,
         ft.SafeArea(
             expand=True,
             content=ft.Container(
                 content=counter,
                 alignment=ft.Alignment.CENTER,
             ),
-        )
+        ),
     )
 
 
